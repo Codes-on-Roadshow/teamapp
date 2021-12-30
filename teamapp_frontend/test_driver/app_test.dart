869e8e9dd69515c3359f51f0c5ce721b48f63862
Ranu Wijaya Putra <ranuwp@gmail.com>
2021-12-30 11:50:49 +0700

    implement behaviour driven development for counter

---
+++ teamapp_frontend/test_driver/app_test.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:gherkin/gherkin.dart';
+import 'steps/given_the_counter_is_string_step.dart';
+import 'steps/when_i_tap_string_button_n_times_step.dart';
+
+Future<void> main() {
+  final config = FlutterTestConfiguration()
+    ..features = [RegExp('features/*.*.feature')]
+    ..reporters = [
+      ProgressReporter(),
+      TestRunSummaryReporter(),
+      JsonReporter(path: './report.json')
+    ] // you can include the "StdoutReporter()" without the message level parameter for verbose log information
+    ..stepDefinitions = [GivenTheCounterIsStringStep(), ITapStringButtonNTimesStep()]
+    ..restartAppBetweenScenarios = true
+    ..targetAppPath = "test_driver/app.dart";
+    // ..tagExpression = "@smoke" // uncomment to see an example of running scenarios based on tag expressions
+  return GherkinRunner().execute(config);
+}
