416823c84718d4383fed80e882e2ec11779ef36f
Ranu WP <ranu.putra@tech.jago.com>
2022-03-14 11:55:44 +0700

    switch to basic integration test

---
+++ teamapp_frontend/integration_test/app_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:team_app/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('should show profile screen', (WidgetTester tester) async {
+      app.main();
+      
+      await tester.pumpAndSettle();
+
+      expect(find.text('Ranu WP'), findsOneWidget);
+      expect(find.text('ranu.wp@team.com'), findsOneWidget);
+      expect(find.byKey(const Key('profile-image')), findsOneWidget);
+    });
+  });
+}
