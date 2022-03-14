41b2ebdb1f6d2e6ebca877e6c5a10b07068d4a9a
Ranu WP <ranu.putra@tech.jago.com>
2022-03-14 13:25:53 +0700

    add missing ensure initialization for integration test

--- teamapp_frontend/integration_test/step/the_app_is_running.dart
+++ teamapp_frontend/integration_test/step/the_app_is_running.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 import 'package:team_app/main.dart' as app;
 
 Future<void> theAppIsRunning(WidgetTester tester) async {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   app.main();
   await tester.pumpAndSettle();
 }
