f7ca28862120a8675e1eb76bf85aca399b2d41b8
Ranu WP <ranu.putra@tech.jago.com>
2022-03-18 10:28:17 +0700

    add post for teams

--- backend/src/main/kotlin/com/dkatalis/teamapp/team/TeamController.kt
+++ backend/src/main/kotlin/com/dkatalis/teamapp/team/TeamController.kt
@@ -3,9 +3,19 @@ package com.dkatalis.teamapp
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 @RestController
 class TeamController {
+    val teams = ArrayList<String>();
+
     @GetMapping("/teams")
-    fun getTeams() = "It's TeamApp"
+    fun getTeams(): List<String> {
+        return teams;
+    }
+
+    @PostMapping("/teams")
+    fun postTeams(team: String) {
+        teams.add(team);
+    }
 }
