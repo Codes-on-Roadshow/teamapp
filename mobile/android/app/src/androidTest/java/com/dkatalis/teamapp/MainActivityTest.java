95eabe9dfd9234ca9df07719a7adf6f4b68fbb33
Ranu WP <ranu.putra@tech.jago.com>
2022-03-30 15:53:23 +0700

    change example package to com.dkatalis.teamapp

--- mobile/android/app/src/androidTest/java/com/dkatalis/teamapp/MainActivityTest.java
+++ mobile/android/app/src/androidTest/java/com/dkatalis/teamapp/MainActivityTest.java
@@ -1,10 +1,10 @@
-package com.example.team_app;
+package com.dkatalis.teamapp;
 
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-import com.example.team_app.MainActivity;
+import com.dkatalis.teamapp.MainActivity;
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
