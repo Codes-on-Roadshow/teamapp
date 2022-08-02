6c1df15e5e678b0c3bfdd3bbe17095520f8588ec
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-08-02 14:32:01 +0700

    add express api

---
+++ apps/api/src/app/todos.ts
@@ -0,0 +1,18 @@
+import { Express } from 'express';
+
+interface Todo {
+  title: string;
+}
+
+const todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+export function addTodoRoutes(app: Express) {
+  app.get('/api/todos', (req, resp) => resp.send(todos));
+  app.post('/api/addTodo', (req, resp) => {
+    const newTodo = {
+      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    };
+    todos.push(newTodo);
+    resp.send(newTodo);
+  });
+}
