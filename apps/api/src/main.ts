ec3325d9ae2afd60eb44bb8b2b12cde33fe21093
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-08-04 20:15:17 +0700

    separate server

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,9 +4,9 @@ import { addPairRoutes } from './app/pairs';
 import { addTeamRoutes } from './app/teams';
 import { addUserRoutes } from './app/users';
 
-const app = express();
-app.use(express.json());
+export const app = express();
 
+app.use(express.json());
 app.get('/api', (_, res) => {
   res.send({ message: 'Welcome to TeamApp!' });
 });
@@ -14,14 +14,3 @@ app.get('/api', (_, res) => {
 addTeamRoutes(app);
 addUserRoutes(app);
 addPairRoutes(app);
-
-const port = process.env.port || 3333;
-
-if (process.env.NODE_ENV !== 'test') {
-  const server = app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}/api`);
-  });
-  server.on('error', console.error);
-}
-
-export { app };
