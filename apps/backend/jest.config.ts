26dcd8f65bad3a14fb434a4f9ea9587ef7b24be2
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-08-08 14:29:15 +0700

    change api to backend

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'api',
+  displayName: 'backend',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/api',
+  coverageDirectory: '../../coverage/apps/backend',
 };
